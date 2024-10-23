60ed732c4c797474a8b95dd87a81ecf060538b06
oVTPo <phatvuongtan0607@gmail.com>
2024-10-23 14:56:22 +0700

    update dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
 import { getFirestore } from "firebase/firestore"; 
 
-// Your web app's Firebase configuration
+// Cấu hình Firebase từ biến môi trường
 const firebaseConfig = {
-  apiKey: "AIzaSyAN_LIqvug6xt9ByoRviUjDmiTy7Ybh9oc",
-  authDomain: "task-management-8a8f9.firebaseapp.com",
-  projectId: "task-management-8a8f9",
-  storageBucket: "task-management-8a8f9.appspot.com",
-  messagingSenderId: "825291689172",
-  appId: "1:825291689172:web:1ec40f3e6119b6dd543f41",
-  measurementId: "G-D8B9M6EM1W"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Khởi tạo ứng dụng Firebase
@@ -23,4 +23,4 @@ const auth = getAuth(app); // Giữ lại khai báo này
 // Khởi tạo Firestore
 const firestore = getFirestore(app); // Giữ lại khai báo này
 
-export { auth, firestore }; // Xuất cả hai biến
+export { auth, firestore }; // Xuất tất cả hai biến
