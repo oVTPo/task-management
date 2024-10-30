602d90eb6b6b54c7bedceb2195782f0dcc6c6c5f
oVTPo <phatvuongtan0607@gmail.com>
2024-10-31 00:34:30 +0700

    Sửa bug kiểm tra một tab đăng nhập

--- client/src/components/hooks/useSingleSession.js
+++ client/src/components/hooks/useSingleSession.js
@@ -1,7 +1,7 @@
-import { getDoc, doc, setDoc, deleteDoc } from 'firebase/firestore';
+import { getDoc, doc, setDoc, deleteDoc, onSnapshot } from 'firebase/firestore';
 import { useEffect } from 'react';
-import { signOut } from 'firebase/auth';
-import { auth, firestore } from '../../firebase'; // Đường dẫn đến cấu hình Firebase của bạn
+import { firestore } from '../../firebase'; // Đường dẫn đến cấu hình Firebase của bạn
+import { v4 as uuidv4 } from 'uuid';
 
 const useSingleSession = (userId) => {
   useEffect(() => {
@@ -11,22 +11,36 @@ const useSingleSession = (userId) => {
     }
 
     const sessionRef = doc(firestore, 'sessions', userId);
-    
-    const checkSession = async () => {
+    const sessionId = uuidv4(); // Tạo session ID duy nhất cho mỗi phiên
+
+    const establishSession = async () => {
       const sessionDoc = await getDoc(sessionRef);
 
       if (!sessionDoc.exists()) {
-        // Nếu không có phiên, tạo một phiên mới
-        await setDoc(sessionRef, { sessionId: 'your-unique-session-id' });
+        // Nếu không có phiên, tạo một phiên mới với sessionId
+        await setDoc(sessionRef, { sessionId });
       } else {
-        // Nếu đã có phiên, thông báo và đăng xuất
-        alert('Tài khoản của bạn đã được đăng nhập trên một thiết bị/tab khác.');
-        
-        window.location.href = '/login';
+        // Nếu đã có phiên khác với sessionId hiện tại, thông báo và điều hướng
+        const currentSessionId = sessionDoc.data().sessionId;
+        if (currentSessionId !== sessionId) {
+          alert('Tài khoản của bạn đã được đăng nhập trên một thiết bị/tab khác.');
+          window.location.href = '/login';
+        }
       }
     };
 
-    checkSession();
+    // Lắng nghe thay đổi của phiên trong Firestore theo thời gian thực
+    const unsubscribe = onSnapshot(sessionRef, (docSnapshot) => {
+      if (docSnapshot.exists()) {
+        const currentSessionId = docSnapshot.data().sessionId;
+        if (currentSessionId !== sessionId) {
+          alert('Tài khoản của bạn đã được đăng nhập trên một thiết bị/tab khác.');
+          window.location.href = '/login';
+        }
+      }
+    });
+
+    establishSession();
 
     // Xử lý khi tắt tab hoặc trình duyệt
     const handleBeforeUnload = async () => {
@@ -38,6 +52,7 @@ const useSingleSession = (userId) => {
     return () => {
       // Dọn dẹp khi component unmount
       window.removeEventListener('beforeunload', handleBeforeUnload);
+      unsubscribe(); // Hủy đăng ký lắng nghe onSnapshot
       deleteDoc(sessionRef); // Xóa phiên khi component unmount
     };
   }, [userId]); // Đảm bảo userId là ID của người dùng đang đăng nhập
